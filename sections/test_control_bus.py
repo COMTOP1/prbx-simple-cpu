f10be5b347fb5951ad8531a6454a9e59aa24b95f
Liam Burnand <liam.burnand@ystv.co.uk>
2024-12-20 15:05:34 +0930

    Add test for control bus

---
+++ sections/test_control_bus.py
@@ -0,0 +1,27 @@
+import unittest
+
+from control_bus import ControlBus, DATA_SEL, ADDR_SEL, IR_EN
+
+
+class TestControlBus(unittest.TestCase):
+    def setUp(self):
+        self.control_bus = ControlBus()
+
+    def test_add_control(self):
+        self.control_bus.add_control(DATA_SEL)
+        self.assertEqual(self.control_bus.read_control_bus(), DATA_SEL)
+
+    def test_add_multiple_control(self):
+        self.control_bus.add_control(ADDR_SEL)
+        self.control_bus.add_control(IR_EN)
+        self.assertEqual(self.control_bus.read_control_bus(), ADDR_SEL | IR_EN)
+
+    def test_clear_control(self):
+        self.control_bus.add_control(ADDR_SEL)
+        self.control_bus.add_control(IR_EN)
+        self.control_bus.clear()
+        self.assertEqual(self.control_bus.read_control_bus(), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
