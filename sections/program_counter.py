f774ca7c5c4f504da6ecc5e7739a4834ec781980
Liam Burnand <liam.burnand@ystv.co.uk>
2024-12-20 15:04:31 +0930

    Accounting for overflow

--- sections/program_counter.py
+++ sections/program_counter.py
@@ -11,6 +11,8 @@ class ProgramCounter:
 
     def enable(self):
         self.__content += 1
+        if self.__content == math.pow(2, 8):
+            self.__content = 0
 
     def get(self):
         return self.__content
