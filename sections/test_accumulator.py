283eb9154684661aa1d382d509385b454001a140
Liam Burnand <liam.burnand@ystv.co.uk>
2024-12-20 15:05:28 +0930

    Add test for accumulator

---
+++ sections/test_accumulator.py
@@ -0,0 +1,21 @@
+import unittest
+
+from accumulator import Accumulator
+
+
+class TestAccumulator(unittest.TestCase):
+    def setUp(self):
+        self.accumulator = Accumulator()
+
+    def test_accumulator_valid(self):
+        tests = {(2, 2), (220, 220)}
+        for inserted_value, expected_value in tests:
+            self.accumulator.insert(inserted_value)
+            self.assertEqual(self.accumulator.get(), expected_value)
+
+    def test_accumulator_error(self):
+        with self.assertRaises(OSError):
+            self.accumulator.insert(256)
+
+if __name__ == '__main__':
+    unittest.main()
