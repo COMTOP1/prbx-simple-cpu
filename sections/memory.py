a7d0c76df647c2972f9556347eecbeb6edcbe8ed
Liam Burnand <liam.burnand@ystv.co.uk>
2024-12-20 16:47:36 +0930

    Fixing the error

--- sections/memory.py
+++ sections/memory.py
@@ -4,18 +4,18 @@ class Memory:
     __memory: list[int] = [0]
 
     def __init__(self, length: int):
-        if length <= 0 or length >= math.pow(2, 16):
+        if length <= 0 or length > math.pow(2, 16):
             raise ValueError(f'Memory length {length} is invalid')
         self.__memory = [0] * length
 
     def insert(self, index: int, value: int):
         if index < 0 or index >= len(self.__memory):
-            raise ValueError(f'Memory index {index} out of range')
+            raise IndexError(f'Memory index {index} out of range of length {len(self.__memory)}')
         if value < 0 or value >= math.pow(2, 8):
             raise ValueError(f'Memory value {value} out of range')
         self.__memory[index] = value
 
     def get(self, index: int):
         if index < 0 or index >= len(self.__memory):
-            raise ValueError(f'Memory index {index} out of range')
+            raise IndexError(f'Memory index {index} out of range')
         return self.__memory[index]
