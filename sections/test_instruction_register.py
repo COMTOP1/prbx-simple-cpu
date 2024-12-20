c6dbebe6a6b9525f3d28eddaf48f56fb4f185935
Liam Burnand <liam.burnand@york.ac.uk>
2024-12-21 07:51:44 +1000

    Add test for instruction register

---
+++ sections/test_instruction_register.py
@@ -0,0 +1,21 @@
+import unittest
+
+from instruction_register import InstructionRegister
+
+
+class TestInstructionRegister(unittest.TestCase):
+    def setUp(self):
+        self.instruction_register = InstructionRegister()
+
+    def test_instruction_register_valid(self):
+        tests = {(2, 2), (817, 817)}
+        for inserted_value, expected_value in tests:
+            self.instruction_register.insert(inserted_value)
+            self.assertEqual(self.instruction_register.get(), expected_value)
+
+    def test_instruction_register_error(self):
+        with self.assertRaises(OSError):
+            self.instruction_register.insert(70000)
+
+if __name__ == '__main__':
+    unittest.main()
