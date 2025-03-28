5f2b7da7dc88eb7747ecbe7035d073b6e8fa78d1
Liam Burnand <liam.burnand@york.ac.uk>
2025-03-28 10:09:27 +0000

    Creating a better parser

--- parser.py
+++ parser.py
@@ -1,9 +1,97 @@
 import re
 
-def raw_parser(raw_asm: str):
+def __raw_parser(raw_asm: str) -> list[str]:
     stripped = (raw_asm + '\n').strip()
     trimmed1 = re.sub('\t+', '\n', stripped)
     trimmed2 = re.sub(' {2,}', ' ', trimmed1)
     trimmed3 = re.sub('\n ', '\n', trimmed2)
     trimmed4 = re.sub('\\s{2,}', ' ', trimmed3)
     return trimmed4.upper().strip().split('\n')
+
+def parser(raw_asm: str) -> list[int]:
+    returning_memory: list[int] = []
+    parsed_list = __raw_parser(raw_asm)
+    for instruction in parsed_list:
+        memory_value: int
+        split_instruction = instruction.split(' ')
+        if split_instruction[0] == "MOVE":
+            if len(split_instruction) != 2:
+                raise ValueError("MOVE instruction must have 2 arguments")
+            value = int(split_instruction[1])
+            if value < 0 or value > 255:
+                raise ValueError("MOVE instruction must be between 0 and 255")
+            memory_value = 0x0000 + value
+        elif split_instruction[0] == "ADD":
+            if len(split_instruction) != 2:
+                raise ValueError("ADD instruction must have 2 arguments")
+            value = int(split_instruction[1])
+            if value < 0 or value > 255:
+                raise ValueError("ADD instruction must be between 0 and 255")
+            memory_value = 0x1000 + value
+        elif split_instruction[0] == "SUB":
+            if len(split_instruction) != 2:
+                raise ValueError("SUB instruction must have 2 arguments")
+            value = int(split_instruction[1])
+            if value < 0 or value > 255:
+                raise ValueError("SUB instruction must be between 0 and 255")
+            memory_value = 0x2000 + value
+        elif split_instruction[0] == "AND":
+            if len(split_instruction) != 2:
+                raise ValueError("AND instruction must have 2 arguments")
+            value = int(split_instruction[1])
+            if value < 0 or value > 255:
+                raise ValueError("AND instruction must be between 0 and 255")
+            memory_value = 0x3000 + value
+        elif split_instruction[0] == "LOAD":
+            if len(split_instruction) != 2:
+                raise ValueError("LOAD instruction must have 2 arguments")
+            value = int(split_instruction[1])
+            if value < 0 or value > 255:
+                raise ValueError("LOAD instruction must be between 0 and 255")
+            memory_value = 0x4000 + value
+        elif split_instruction[0] == "STORE":
+            if len(split_instruction) != 2:
+                raise ValueError("STORE instruction must have 2 arguments")
+            value = int(split_instruction[1])
+            if value < 0 or value > 255:
+                raise ValueError("STORE instruction must be between 0 and 255")
+            memory_value = 0x5000 + value
+        elif split_instruction[0] == "ADDM":
+            if len(split_instruction) != 2:
+                raise ValueError("ADMM instruction must have 2 arguments")
+            value = int(split_instruction[1])
+            if value < 0 or value > 255:
+                raise ValueError("ADDM instruction must be between 0 and 255")
+            memory_value = 0x6000 + value
+        elif split_instruction[0] == "SUBM":
+            if len(split_instruction) != 2:
+                raise ValueError("SUBM instruction must have 2 arguments")
+            value = int(split_instruction[1])
+            if value < 0 or value > 255:
+                raise ValueError("SUBM instruction must be between 0 and 255")
+            memory_value = 0x7000 + value
+        elif split_instruction[0] == "JUMPU":
+            if len(split_instruction) != 2:
+                raise ValueError("JUMPU instruction must have 2 arguments")
+            value = int(split_instruction[1])
+            if value < 0 or value > 255:
+                raise ValueError("JUMPU instruction must be between 0 and 255")
+            memory_value = 0x8000 + value
+        elif split_instruction[0] == "JUMPZ":
+            if len(split_instruction) != 2:
+                raise ValueError("JUMPZ instruction must have 2 arguments")
+            value = int(split_instruction[1])
+            if value < 0 or value > 255:
+                raise ValueError("JUMPZ instruction must be between 0 and 255")
+            memory_value = 0x9000 + value
+        elif split_instruction[0] == "JUMPNZ":
+            if len(split_instruction) != 2:
+                raise ValueError("JUMPNZ instruction must have 2 arguments")
+            value = int(split_instruction[1])
+            if value < 0 or value > 255:
+                raise ValueError("JUMPNZ instruction must be between 0 and 255")
+            memory_value = 0xA000 + value
+        else:
+            raise ValueError(f"Unsupported instruction: {split_instruction}")
+        returning_memory.append(memory_value)
+    return returning_memory
