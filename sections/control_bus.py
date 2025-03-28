d5b323cad8e3a0e43a770f0ae02f8d04086962bf
Liam Burnand <liam.burnand@york.ac.uk>
2025-03-28 10:03:22 +0000

    Added internal control lines and prefixed all values with two 0s so doesn't affect anything else

--- sections/control_bus.py
+++ sections/control_bus.py
@@ -1,48 +1,48 @@
 type ControlLine = int
 
-PC_EN: ControlLine =    0B100000000000
+PC_EN: ControlLine =    0B00100000000000
 """
 PC_EN - Program Counter enable
 
 The Program Counter outputs to the Address Bus
 """
-PC_LD: ControlLine =    0B010000000000
+PC_LD: ControlLine =    0B00010000000000
 """
 PC_LD - Program Counter load
 
 The Program Counter outputs to the Address Bus mux
 """
-IR_EN: ControlLine =    0B001000000000
+IR_EN: ControlLine =    0B00001000000000
 """
 IR_EN - Instruction Register enable
 
 The Program Counter outputs to the Internal Bus
 """
-ACC_EN: ControlLine =   0B000100000000
+ACC_EN: ControlLine =   0B00000100000000
 """
 ACC_EN - Accumulator enable
 
 The Accumulator outputs to the Data In Bus
 """
-ACC_CTL2: ControlLine = 0B000010000000
+ACC_CTL2: ControlLine = 0B00000010000000
 """
 ACC_CTL2 - ALU control line 2
 
 The control line 2 is part of ALU control
 """
-ACC_CTL1: ControlLine = 0B000001000000
+ACC_CTL1: ControlLine = 0B00000001000000
 """
 ACC_CTL1 - ALU control line 1
 
 The control line 1 is part of ALU control
 """
-ACC_CTL0: ControlLine = 0B000000100000
+ACC_CTL0: ControlLine = 0B00000000100000
 """
 ACC_CTL0 - ALU control line 0
 
 The control line 0 is part of ALU control
 """
-ADDR_SEL: ControlLine = 0B000000010000
+ADDR_SEL: ControlLine = 0B00000000010000
 """
 ADDR_SEL - Address mux selector
 
@@ -50,7 +50,7 @@ Signal 0 passes the Program Counter to the Address Bus
 
 Signal 1 passes the Internal Bus to the Address Bus
 """
-DATA_SEL: ControlLine = 0B000000001000
+DATA_SEL: ControlLine = 0B00000000001000
 """
 DATA_SEL - Data mux selector
 
@@ -58,23 +58,32 @@ Signal 0 passes the Internal Bus to the ALU
 
 Signal 1 passes the Data Out Bus to the ALU
 """
-RAM_EN: ControlLine =   0B000000000100
+RAM_EN: ControlLine =   0B00000000000100
 """
 RAM_EN - RAM enable
 
 The RAM outputs to the Data Out Bus
 """
-RAM_WR: ControlLine =   0B000000000010
+RAM_WR: ControlLine =   0B00000000000010
 """
 RAM_WR - RAM write enable
 
 The RAM accepts data in from the Data In Bus
 """
-ROM_EN: ControlLine =   0B000000000001
+ROM_EN: ControlLine =   0B00000000000001
 """
 ROM_EN - ROM enable
 
 The RAM outputs to the Data Out Bus (not used)
+"""
+
+ZERO_FLAG: ControlLine =    0B01000000000000
+"""
+ZERO_FLAG - Internal zero flag
+"""
+NOT_ZERO_FLAG: ControlLine =    0B10000000000000
+"""
+NOT_ZERO_FLAG - Internal not zero flag
 """
 
 class ControlBus:
