a872afa17b7d45c8771aa9a6d76a88e3b5b9599e
Liam Burnand <liam.burnand@york.ac.uk>
2025-04-10 11:17:15 +0100

    Adding hex values and making sure all the values are unique and make sense

--- sections/control_bus.py
+++ sections/control_bus.py
@@ -15,7 +15,7 @@ ZERO_FLAG - Internal zero flag (0X4000)
 
 PC_EN: ControlLine =            0B00010000000000000
 """
-PC_EN - Program Counter enable
+PC_EN - Program Counter enable (0X2000)
 
 The Program Counter outputs to the Address Bus
 """
@@ -27,7 +27,7 @@ The Program Counter will increment it's value
 """
 PC_LD: ControlLine =            0B00000100000000000
 """
-PC_LD - Program Counter load
+PC_LD - Program Counter load (0X800)
 
 The Program Counter outputs to the Address Bus mux
 """
@@ -37,9 +37,9 @@ IR_WR - Instruction Register write enable (0X400)
 
 The Instruction Register can be written to
 """
-ACC_EN: ControlLine =   0B00000100000000
+ACC_EN: ControlLine =           0B00000001000000000
 """
-ACC_EN - Accumulator enable
+ACC_EN - Accumulator enable (0X200)
 
 The Accumulator outputs to the Data In Bus
 """
@@ -51,53 +51,53 @@ The Accumulator can be written to
 """
 ACC_CTL2: ControlLine =         0B00000000010000000
 """
-ACC_CTL2 - ALU control line 2
+ACC_CTL2 - ALU control line 2 (0X80)
 
 The control line 2 is part of ALU control
 """
-ACC_CTL1: ControlLine = 0B00000001000000
+ACC_CTL1: ControlLine =         0B00000000001000000
 """
-ACC_CTL1 - ALU control line 1
+ACC_CTL1 - ALU control line 1 (0X40)
 
 The control line 1 is part of ALU control
 """
-ACC_CTL0: ControlLine = 0B00000000100000
+ACC_CTL0: ControlLine =         0B00000000000100000
 """
-ACC_CTL0 - ALU control line 0
+ACC_CTL0 - ALU control line 0 (0X20)
 
 The control line 0 is part of ALU control
 """
-ADDR_SEL: ControlLine = 0B00000000010000
+ADDR_SEL: ControlLine =         0B00000000000010000
 """
-ADDR_SEL - Address mux selector
+ADDR_SEL - Address mux selector (0X10)
 
 Signal 0 passes the Program Counter to the Address Bus (unset)
 
 Signal 1 passes the Internal Bus to the Address Bus
 """
-DATA_SEL: ControlLine = 0B00000000001000
+DATA_SEL: ControlLine =         0B00000000000001000
 """
-DATA_SEL - Data mux selector
+DATA_SEL - Data mux selector (0X8)
 
 Signal 0 passes the Internal Bus to the ALU (unset)
 
 Signal 1 passes the Data Out Bus to the ALU
 """
-RAM_EN: ControlLine =   0B00000000000100
+RAM_EN: ControlLine =           0B00000000000000100
 """
-RAM_EN - RAM enable
+RAM_EN - RAM enable (0X4)
 
 The RAM outputs to the Data Out Bus
 """
-RAM_WR: ControlLine =   0B00000000000010
+RAM_WR: ControlLine =           0B00000000000000010
 """
-RAM_WR - RAM write enable
+RAM_WR - RAM write enable (0X2)
 
 The RAM accepts data in from the Data In Bus
 """
-ROM_EN: ControlLine =   0B00000000000001
+ROM_EN: ControlLine =           0B00000000000000001
 """
-ROM_EN - ROM enable
+ROM_EN - ROM enable (0X1)
 
 The RAM outputs to the Data Out Bus (not used)
 """
