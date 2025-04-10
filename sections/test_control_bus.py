0512dfb457544188654fdd2a064558179ae4ff0a
Liam Burnand <liam.burnand@york.ac.uk>
2025-04-10 11:18:40 +0100

    Fixing test

--- sections/test_control_bus.py
+++ sections/test_control_bus.py
@@ -1,6 +1,6 @@
 import unittest
 
-from sections.control_bus import ControlBus, DATA_SEL, ADDR_SEL, IR_EN
+from sections.control_bus import ControlBus, DATA_SEL, ADDR_SEL, IR_WR
 
 
 class TestControlBus(unittest.TestCase):
@@ -13,12 +13,12 @@ class TestControlBus(unittest.TestCase):
 
     def test_add_multiple_control(self):
         self.control_bus.add_control(ADDR_SEL)
-        self.control_bus.add_control(IR_EN)
-        self.assertEqual(self.control_bus.read_control_bus(), ADDR_SEL | IR_EN)
+        self.control_bus.add_control(IR_WR)
+        self.assertEqual(self.control_bus.read_control_bus(), ADDR_SEL | IR_WR)
 
     def test_clear_control(self):
         self.control_bus.add_control(ADDR_SEL)
-        self.control_bus.add_control(IR_EN)
+        self.control_bus.add_control(IR_WR)
         self.control_bus.clear()
         self.assertEqual(self.control_bus.read_control_bus(), 0)
 
