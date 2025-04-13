1405da5e9af53c4a4a1a4d9a58dd124a2ae7a3be
Liam Burnand <liam.burnand@york.ac.uk>
2025-04-13 01:27:14 +0100

    Reformatting file

--- gui/cpu_blocks.py
+++ gui/cpu_blocks.py
@@ -11,7 +11,8 @@ class CPUBlockDiagram:
         self.canvas.create_rectangle(x, y, x + w, y + h, fill=colour, width=2)
 
         text = label if initial_value == "" else f"{label}({initial_value})"
-        label_id = self.canvas.create_text(x + w / 2, y + h / 2, text=text, font=("Arial", 18), fill=font_colour, justify=tk.CENTER)
+        label_id = self.canvas.create_text(x + w / 2, y + h / 2, text=text, font=("Arial", 18), fill=font_colour,
+                                           justify=tk.CENTER)
 
         self.component_labels_initial[name] = label
         self.component_labels[name] = label_id
@@ -22,29 +23,36 @@ class CPUBlockDiagram:
 
     def draw_all_blocks(self):
         self.draw_block("ACC", "Accumulator\nACC ", 250, 550, initial_value="0x00")
-        self.draw_block("IR", "Instruction Register\nIR ", 100, 50, initial_value="0x0000", colour="slate blue")
-        self.draw_block("PC", "Program Counter\nPC ", 525, 200, initial_value="0x00", colour="maroon")
+        self.draw_block("IR", "Instruction Register\nIR ", 100, 50, initial_value="0x0000",
+                        colour="slate blue")
+        self.draw_block("PC", "Program Counter\nPC ", 525, 200, initial_value="0x00",
+                        colour="maroon")
         self.draw_block("ALU", "Arithmetic Logic Unit\nALU", 250, 400, colour="green")
-        self.draw_block("CONTROL_LOGIC", "Control Logic\n(Lines Below)", 50, 300, w=120, h=70, colour="grey")
+        self.draw_block("CONTROL_LOGIC", "Control Logic\n(Lines Below)", 50, 300, w=120, h=70,
+                        colour="grey")
         self.draw_block("MEMORY", "Memory\n(Right side)", 750, 150, w=120, h=400, colour="grey")
         self.draw_block("ADDR_MUX", "Address\nMux ", 550, 325, w=80, h=120,
                         colour="medium purple", mux=True, x0=555, y0=340, x1=555, y1=430, initial_value="0")
         self.draw_block("DATA_MUX", "Data\nMux ", 375, 325, w=80, h=50,
                         colour="medium purple", mux=True, x0=390, y0=330, x1=435, y1=330, initial_value="0")
-        self.draw_block("ZERO", "Zero flag\n", 500, 550, initial_value="False", w=80, h=50, colour="navy")
+        self.draw_block("ZERO", "Zero flag\n", 500, 550, initial_value="False", w=80, h=50,
+                        colour="navy")
 
-
-        self.draw_block("DATA_OUT_BUS", "Data Out\n", 850, 50, initial_value="0x0000", w=80, h=50, colour="deep sky blue")
-        self.draw_block("DATA_IN_BUS", "Data In\n", 850, 600, initial_value="0x0000", w=80, h=50, colour="dark blue")
-        self.draw_block("INTERNAL_BUS", "Internal\n", 200, 200, initial_value="0x0000", w=80, h=50, colour="purple")
-        self.draw_block("ADDR_BUS", "Addr Bus\n", 640, 400, initial_value="0x00", w=80, h=50, colour="lawn green",
-                        font_colour="black")
-        self.draw_block("ALU_ACC", "ALU>ACC\n", 370, 485, initial_value="0x00", w=100, h=50, colour="sky blue",
-                        font_colour="black")
+        self.draw_block("DATA_OUT_BUS", "Data Out\n", 850, 50, initial_value="0x0000", w=80, h=50,
+                        colour="deep sky blue", font_colour="black")
+        self.draw_block("DATA_IN_BUS", "Data In\n", 850, 600, initial_value="0x0000", w=80, h=50,
+                        colour="dark blue")
+        self.draw_block("INTERNAL_BUS", "Internal\n", 200, 200, initial_value="0x0000", w=80, h=50,
+                        colour="purple")
+        self.draw_block("ADDR_BUS", "Addr Bus\n", 640, 400, initial_value="0x00", w=80, h=50,
+                        colour="lawn green", font_colour="black")
+        self.draw_block("ALU_ACC", "ALU>ACC\n", 370, 485, initial_value="0x00", w=100, h=50,
+                        colour="sky blue", font_colour="black")
 
     def update_block_value(self, values: list[tuple[str, str]]):
         i = 0
         while i < len(values):
             name, value = values[i]
-            self.canvas.itemconfigure(self.component_labels[name], text=f"{self.component_labels_initial[name]}({value})")
+            self.canvas.itemconfigure(self.component_labels[name],
+                                      text=f"{self.component_labels_initial[name]}({value})")
             i += 1
